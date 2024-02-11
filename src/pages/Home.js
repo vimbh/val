f0f2b82d7f5b43b913bd6228d5353d911e57637e
Vimukthi Herath <vimukthibh96@gmail.com>
2024-02-12 01:00:57 +1100

    modified home page to play around w ideads

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,128 +1,166 @@
 import "../components/css/GameContainer.css";
 import "../components/css/Layout.css";
-import { useState } from "react";
-
+import image from '../assets/images/cat.jpeg';
+import image2 from '../assets/images/cato2.jpeg';
 
 function Home() {
 
-    const [gridSize, setGridSize] = useState(3);
+    const tileDim = 300;
 
-    // Config grid
-    const handleGridSize = (e) => {
-        const size = parseInt(e.target.value)
+    const fun = () => {
+        const n = 3
 
-        if (!isNaN(size) && size > 0) {
-            setGridSize(size)
-        } else {
-            // Default grid is 3x3
-            setGridSize(3);
-        }
-    }
-    
-    function validMove(e) {
-
-        const state = e.target.classList[1]
-        const i = parseInt(e.target.id)
-        console.log(i)
-
-        if (state === "fixed") {
-            return false
-        } 
-
-        // LHS
-        if ( i % gridSize !== 1) {
-            console.log("can move left")
-        } 
-
-        // RHS
-        if (i % gridSize !== 0) {
-            console.log("can move right")
-        }
-
-        // TOP
-        if ( i > gridSize ) {
-            console.log("can move up")
-        }
-
-        // BOT
-        if ( i < (gridSize**2 - (gridSize + 1)) ) {
-            console.log("can move down")
-        }
-
-    }
-
-    // Move tiles on click
-    function moveTile(e) {
-        console.log(e.target.id)
-        
-        if (validMove(e)) {
-            console.log("valid")
-        } else {
-            console.log("stuck")
-        }
-
-
-    }
-
-    function createGrid() {
-        
-        const gridContainer = []
-        const grid = []
-        let gridTile = []
-        let gridId = 1
-
-        // Pick a random tile to set free b.w [1, n^2]
-        const freeTile = Math.floor(Math.random() * gridSize**2) + 1
-
-        // Create grid element
-        for (let i = 0; i < gridSize; i++) {
-            gridTile = []
-            for (let j = 0; j < gridSize; j++) {
-                gridId = j + 1 + i*gridSize
-                gridTile.push(
-                    <div 
-                    className={`grid-object ${gridId === freeTile ? 'free' : 'fixed'}`}
-                    onClick={moveTile}
-                    id={gridId}
-                    key={gridId}>
-                        {gridId}
-                    </div>
-                )
+        for (var i=0; i < n; i++) {
+            for (var j=0; j < n; j++) {
+                console.log(`${-100*j}px ${-100*i}px`);
             }
-            // Create grid row
-            grid.push(
-                <div 
-                className="grid-row"
-                id={`gridRow-${i+1}`}
-                key={`gridRow-${i+1}`}
-                >
-                    {gridTile}
-                </div>
-            )
+            
         }
-
-        // Create grid container
-        gridContainer.push(
-            <div className="grid-container">
-                {grid}
-            </div>
-        )
-
-        return gridContainer
     }
+
+    fun();
+
     
+
 
     return (
 
-       <div>
-            <input onChange={handleGridSize}/>
-            {gridSize}
-                
-            <div className="center">
-                {createGrid()}
-            </div>
-                
+        <>
+
+        <div 
+            style={{ display: 'flex',
+                     flexDirection: 'column', 
+                     backgroundImage: `url(${image2})`,
+                     width: `${tileDim}px`,
+                     height: `${tileDim}px`,
+                     backgroundSize: '100%',
+                     backgroundRepeat: 'no-repeat' }}>
+        
+
+        <div style={{display: 'flex'}}>
+
+            <div
+                style={{
+                    // border: '1px solid black',
+                    width: '100px',
+                    height: '100px',
+                    backgroundSize: `${tileDim}%`,
+                    backgroundImage: `url(${image2})`,
+                    backgroundPosition: '0px 0px',
+                    backgroundRepeat: 'no-repeat',
+
+                }}
+            />
+            <div
+                style={{
+                    // border: '1px solid black',
+                    width: '100px',
+                    height: '100px',
+                    backgroundSize: `${tileDim}%`,
+                    backgroundImage: `url(${image2})`,
+                    backgroundPosition: '-100px 0px',
+                    backgroundRepeat: 'no-repeat',
+
+                }}
+            />
+            <div
+                style={{
+                    // border: '1px solid black',
+                    width: '100px',
+                    height: '100px',
+                    backgroundSize: `${tileDim}%`,
+                    backgroundImage: `url(${image2})`,
+                    backgroundPosition: '-200px 0px',
+                    backgroundRepeat: 'no-repeat',
+                }}
+            />
+
         </div>
+
+        <div style={{display: 'flex'}}>
+
+            <div
+                style={{
+                    // border: '1px solid black',
+                    width: '100px',
+                    height: '100px',
+                    backgroundSize: `${tileDim}%`,
+                    backgroundImage: `url(${image2})`,
+                    backgroundPosition: '0px -100px',
+                    backgroundRepeat: 'no-repeat',
+                }}
+            />
+
+            <div
+                style={{
+                    // border: '1px solid black',
+                    width: '100px',
+                    height: '100px',
+                    backgroundSize: `${tileDim}%`,
+                    backgroundImage: `url(${image2})`,
+                    backgroundPosition: '-100px -100px',
+                    backgroundRepeat: 'no-repeat',
+                }}
+            />
+
+            <div
+                style={{
+                    // border: '1px solid black',
+                    width: '100px',
+                    height: '100px',
+                    backgroundSize: `${tileDim}%`,
+                    backgroundImage: `url(${image2})`,
+                    backgroundPosition: '-200px -100px',
+                    backgroundRepeat: 'no-repeat',
+                }}
+            />
+
+        </div>
+
+        <div style={{display: 'flex'}}>
+
+            <div
+                style={{
+                    // border: '1px solid black',
+                    width: '100px',
+                    height: '100px',
+                    backgroundSize: `${tileDim}%`,
+                    backgroundImage: `url(${image2})`,
+                    backgroundPosition: '0px -200px',
+                    backgroundRepeat: 'no-repeat',
+                }}
+            />
+
+            <div
+                style={{
+                    // border: '1px solid black',
+                    width: '100px',
+                    height: '100px',
+                    backgroundSize: `${tileDim}%`,
+                    backgroundImage: `url(${image2})`,
+                    backgroundPosition: '-100px -200px',
+                    backgroundRepeat: 'no-repeat',
+                }}
+            />
+
+            <div
+                style={{
+                    border: '1px solid black',
+                    width: '100px',
+                    height: '100px',
+                    backgroundSize: `${tileDim}%`,
+                    backgroundImage: `url(${image2})`,
+                    backgroundPosition: '-200px -200px',
+                    backgroundRepeat: 'no-repeat',
+                }}
+            />
+
+            </div>
+
+        </div>  
+
+    </>
+      
     );
 }
 
